e2271b65904e1b91293dbab3a75b9273df053767
laiba12-cloud <laibayounis123@gmail.com>
2025-09-12 10:26:01 +0500

    updated code

---
+++ lib/controller/components/blacktextwidget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BlackTextWidget extends StatelessWidget {
+  final String text;
+  const BlackTextWidget({super.key,required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,style: GoogleFonts.poppins(
+          fontWeight: FontWeight.w500,
+          fontSize:15,
+        ),
+    );
+  }
+}
